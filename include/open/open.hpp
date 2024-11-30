2d3f69d3463beddd030fdc9d8797e58284d10c17
Jules Fouchy <jules.fouchy@ntymail.com>
2024-11-30 14:31:49 +0100

    🤏 Add overload that takes a std::filesystem::path

--- include/open/open.hpp
+++ include/open/open.hpp
@@ -1,8 +1,14 @@
 #pragma once
+#include <filesystem>
 
 namespace Cool {
 
 /// Opens the link in the user's default web browser, or the folder in the folder explorer, or the file in the default configured software.
 void open(const char* link_or_path);
+/// Opens the link in the user's default web browser, or the folder in the folder explorer, or the file in the default configured software.
+inline void open(std::filesystem::path const& path)
+{
+    open(path.string().c_str());
+}
 
 } // namespace Cool
